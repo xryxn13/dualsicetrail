61568dd799f1f1394283a449b69dd51dfd00c08a
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-23 01:01:52 +0530

    Update simplesidebysidevideo.py

--- simplesidebysidevideo.py
+++ simplesidebysidevideo.py
@@ -1,24 +1,14 @@
 import streamlit as st
-from PIL import Image, ImageSequence
+from PIL import Image
 
 def main():
     st.title("GIF Viewer")
 
-    # Display the GIF files and stop at the last frame
+    # Display the first frame of each GIF
     for i in range(1, 6):
         gif_path = f"{i}.gif"
-        gif_frames = Image.open(gif_path)
-        
-        # Extract frames from the GIF
-        frames = [frame.copy() for frame in ImageSequence.Iterator(gif_frames)]
-
-        # Display each frame of the GIF
-        for frame in frames[:-1]:  # Exclude the last frame
-            st.image(frame, use_column_width=True)
-
-        # Display the last frame separately
-        st.image(frames[-1], use_column_width=True)
-        st.write(f"Video {i}")
+        first_frame = Image.open(gif_path)
+        st.image(first_frame, use_column_width=True, caption=f"Video {i}")
         st.write("---")
         st.write(" ")
         st.write(" ")
