880c7955e796fb345c746a1d648cebb5e679b8e5
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-23 00:56:16 +0530

    Update simplesidebysidevideo.py

--- simplesidebysidevideo.py
+++ simplesidebysidevideo.py
@@ -3,9 +3,9 @@ import streamlit as st
 def main():
     st.title("GIF Viewer")
 
-    # Display the GIF files in a row
+    # Display the GIF files in a column without captions
     for i in range(1, 6):
-        st.image(f"{i}.gif", caption=f"Video {i}", use_column_width=True)
+        st.image(f"{i}.gif", use_column_width=True)
 
 if __name__ == "__main__":
     main()
