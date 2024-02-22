329f88a041cb5ce1937b4882359c4f26b023b6c9
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-23 00:58:32 +0530

    Update simplesidebysidevideo.py

--- simplesidebysidevideo.py
+++ simplesidebysidevideo.py
@@ -7,12 +7,12 @@ def main():
     # Display the GIF files and stop at the last frame
     for i in range(1, 6):
         gif_path = f"{i}.gif"
-        st.image(gif_path, use_column_width=True, format='GIF')
+        st.image(gif_path, use_column_width=True)
 
         # Open the GIF file using PIL
         with Image.open(gif_path) as img:
             # Get the last frame of the GIF
-            last_frame = img.seek(img.n_frames - 1)
+            img.seek(img.n_frames - 1)
             # Convert the last frame to a static image
             last_frame_img = img.convert("RGB")
             # Display the last frame as a static image
