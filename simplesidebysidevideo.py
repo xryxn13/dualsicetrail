1bacfe8825dfb6c6b1ca6a1ff6dcdefb64bac006
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-23 01:05:32 +0530

    Update simplesidebysidevideo.py

--- simplesidebysidevideo.py
+++ simplesidebysidevideo.py
@@ -6,7 +6,7 @@ def main():
     # Display the GIF files in a column
     for i in range(1, 6):
         gif_path = f"{i}.gif"
-        st.image(gif_path, use_column_width=True, caption=f"Video {i}", format='GIF')
+        st.image(gif_path, use_column_width=True, caption=f"Video {i}")
 
 if __name__ == "__main__":
     main()
